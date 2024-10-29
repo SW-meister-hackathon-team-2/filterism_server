bb2215783d9894385327a3db65b1d8d4cce12b33
101 <tmddnjs001133@dsm.hs.kr>
2024-10-30 08:49:33 +0900

    feat: 팬딩 상태 추가

--- src/main/java/com/group2/filterism/template/vo/AccessScope.java
+++ src/main/java/com/group2/filterism/template/vo/AccessScope.java
@@ -1,6 +1,16 @@
 package com.group2.filterism.template.vo;
 
 public enum AccessScope {
+    /**
+     * 심사를 거친 상태
+     */
     PUBLIC,
+    /**
+     * 심사 대기 중
+     */
+    PENDING,
+    /**
+     * 비동개
+     */
     PRIVATE
 }
