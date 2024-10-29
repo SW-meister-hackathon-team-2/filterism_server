3d656f5c0a9c25eef29591f0a39011259d03a538
cabbage16 <qocn16@gmail.com>
2024-10-29 15:16:16 +0900

    initial commit

---
+++ src/main/java/com/group2/filterism/FilterismApplication.java
@@ -0,0 +1,13 @@
+package com.group2.filterism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group2/filterism/FilterismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.filterism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
