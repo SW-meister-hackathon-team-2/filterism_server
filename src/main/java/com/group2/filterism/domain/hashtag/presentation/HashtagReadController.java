9b1939fe5cd4bdfdba14dfd21448a2983d74a06a
cabbage16 <qocn16@gmail.com>
2024-10-30 13:40:07 +0900

    refactor: Http Response 패키지 이동 - http 패키지의 위치를 global로 변경했어요

--- src/main/java/com/group2/filterism/domain/hashtag/presentation/HashtagReadController.java
+++ src/main/java/com/group2/filterism/domain/hashtag/presentation/HashtagReadController.java
@@ -1,7 +1,7 @@
 package com.group2.filterism.domain.hashtag.presentation;
 
 import com.group2.filterism.domain.hashtag.application.HashtagReadUseCase;
-import com.group2.filterism.http.ListResponse;
+import com.group2.filterism.global.http.ListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
